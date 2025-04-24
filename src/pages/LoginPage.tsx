a02b77abfce09c00eaeba5f5b52732ee9cef8d41
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2025-04-24 14:02:43 +0530

    api fixed

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
 
     try {
       const response = await axios.post<Record<string, any>>(
-        `${import.meta.env.VITE_IMANAGER_MAIN_API}/api/v1/org/login`,
+        'https://imanager.duckdns.org/api/v1/org/login',
         payload,
             { // Ensure cookies and credentials are sent
       headers: {
